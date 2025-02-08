fe8df22d32222282dbbeb541ebddb09f8ce60fbd
subframe7536 <1667077010@qq.com>
2025-02-08 15:47:13 +0800

    upgrade and improve github link

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { splitProps } from 'solid-js'
 
 export const buttonVariants = clsvDefault(
   clsv(
-    'inline-flex items-center justify-center rounded-md text-sm font-medium transition-[color,background-color,box-shadow] fv-effect disabled:(pointer-events-none opacity-50) select-none',
+    'inline-flex items-center justify-center rounded-md text-sm font-medium transition fv-effect disabled:(pointer-events-none opacity-50) select-none',
     {
       variant: {
         default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
