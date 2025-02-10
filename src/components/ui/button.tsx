a5f707c7fbe3f850edd3d8f9788c71d9e05f22ed
subframe7536 <1667077010@qq.com>
2025-02-10 23:53:36 +0800

    update hero

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { splitProps } from 'solid-js'
 
 export const buttonVariants = clsvDefault(
   clsv(
-    'inline-flex items-center justify-center rounded-md text-sm font-medium transition fv-effect disabled:(pointer-events-none opacity-50) select-none whitespace-nowrap',
+    'inline-flex items-center justify-center rounded-md font-medium transition fv-effect disabled:(pointer-events-none opacity-50) select-none whitespace-nowrap',
     {
       variant: {
         default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
@@ -16,10 +16,10 @@ export const buttonVariants = clsvDefault(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2',
+        default: 'h-9 px-4 py-2 text-sm',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
-        icon: 'h-9 w-9',
+        icon: 'size-9',
       },
     },
   ),
