8a9fa3ed406731a0d11c43d5a398465fc4a422ad
subframe7536 <1667077010@qq.com>
2025-03-12 20:56:02 +0800

    update showcase text and font url

--- src/cdn.ts
+++ src/cdn.ts
@@ -5,11 +5,16 @@ export const isDEV = import.meta.env.DEV ?? process?.env.NODE_ENV === 'developme
 const fontPrefix = isDEV ? `/fonts` : `${myGhCdnPrefix}/maple-font@variable/woff2/var`
 
 function loadMapleMonoFontFace(italic: boolean) {
+  const suffix = italic ? 'Italic' : 'Regular'
   const src = [
+    `url('${fontPrefix}/test.woff2') format('woff2-variations')`,
     `url('${fontPrefix}/MapleMono${italic ? '-Italic' : ''}[wght]-VF.woff2') format('woff2-variations')`,
-    'local("Maple Mono")',
-    'local("Maple Mono NF")',
-    'local("Maple Mono NF CN")',
+    `local("MapleMono-NF-CN-${suffix}")`,
+    `local("Maple Mono NF CN ${suffix}")`,
+    `local("MapleMono-NF-${suffix}")`,
+    `local("Maple Mono NF ${suffix}")`,
+    `local("MapleMono-${suffix}")`,
+    `local("Maple Mono ${suffix}")`,
   ].join(',')
   return new FontFace('MapleMono', src, {
     display: 'swap',
