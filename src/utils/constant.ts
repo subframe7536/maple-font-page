9dba0426116978319e1fadc36ebadfb9b12905df
subframe7536 <1667077010@qq.com>
2025-03-16 22:27:37 +0800

    update cdn url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,7 +2,7 @@ export const cdnPrefix = 'https://esm.sh'
 export const myGhCdnPrefix = `${cdnPrefix}/gh/subframe7536`
 export const isDEV = import.meta.env.DEV ?? process?.env.NODE_ENV === 'development'
 
-export const fontPrefix = isDEV ? `/fonts` : `${myGhCdnPrefix}/maple-font@variable/woff2/var`
+export const fontPrefix = isDEV ? `/fonts` : `${myGhCdnPrefix}/maple-font@v7.0/woff2/var`
 
 export const DEFAULT_LOCALE: string = 'en'
 
