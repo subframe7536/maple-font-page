d0ac287e3d3f841ec6a2d6cc3ce32e543b4eabe0
subframe7536 <1667077010@qq.com>
2025-04-22 19:28:04 +0800

    update prod font url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,8 +1,12 @@
+import { version } from '../../package.json'
+
 export const cdnPrefix = 'https://esm.sh'
 export const myGhCdnPrefix = `${cdnPrefix}/gh/subframe7536`
 export const isDEV = import.meta.env.DEV ?? process?.env.NODE_ENV === 'development'
 
-export const fontPrefix = isDEV ? `/fonts` : `${myGhCdnPrefix}/maple-font@v7.0/woff2/var`
+export const fontPrefix = isDEV
+  ? `/fonts`
+  : `${myGhCdnPrefix}/maple-font@v${version.split('.', 2).join('.')}/woff2/var`
 
 export const DEFAULT_LOCALE: string = 'en'
 
