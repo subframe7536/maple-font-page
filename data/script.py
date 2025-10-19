7bd220585cfda9d298fd4cde2e920534fb7d44e9
subframe7536 <1667077010@qq.com>
2025-10-19 19:20:45 +0800

    Update landing page data

--- data/script.py
+++ data/script.py
@@ -11,24 +11,24 @@ def get_freeze_config_str(config):
 		if C=='0'and B==_A or C=='-1':A+=f"-{B};"
 	return A
 def freeze_feature(font,moving_rules,config):
-	R='GSUB';I=config;A=font;J=A[R].table.FeatureList;K=J.FeatureRecord;L={A.FeatureTag:(B,A.Feature)for(B,A)in enumerate(K)if A.FeatureTag!=_A};M=[A.Feature for A in K if A.FeatureTag==_A]
-	if I.get(_A)!='1':
+	S='GSUB';I=config;A=font;J=A[S].table.FeatureList;K=J.FeatureRecord;L={A.FeatureTag:(B,A.Feature)for(B,A)in enumerate(K)if A.FeatureTag!=_A};M=[A.Feature for A in K if A.FeatureTag==_A];N=I.get(_A)=='1'
+	if not N:
 		for B in M:B.LookupListIndex.clear();B.LookupCount=0
-	N=[]
-	for(G,O)in I.items():
-		if G not in L or O=='0':continue
-		H,P=L[G]
-		if O=='-1':N.append(H);continue
-		if G in moving_rules:
-			for B in M:B.LookupListIndex.extend(P.LookupListIndex)
+	O=[]
+	for(G,P)in I.items():
+		if G not in L or P=='0':continue
+		H,Q=L[G]
+		if P=='-1':O.append(H);continue
+		if G in moving_rules and N:
+			for B in M:B.LookupListIndex.extend(Q.LookupListIndex)
 		else:
 			C=A['glyf'].glyphs;D=A['hmtx'].metrics
-			for S in P.LookupListIndex:
-				Q=getattr(A[R].table.LookupList.Lookup[S].SubTable[0],'mapping',None)
-				if not Q:continue
-				for(E,F)in Q.items():
+			for T in Q.LookupListIndex:
+				R=getattr(A[S].table.LookupList.Lookup[T].SubTable[0],'mapping',None)
+				if not R:continue
+				for(E,F)in R.items():
 					if E in C and E in D and F in C and F in D:C[E]=C[F];D[E]=D[F]
-	for H in sorted(N,reverse=True):del J.FeatureRecord[H]
+	for H in sorted(O,reverse=True):del J.FeatureRecord[H]
 def set_font_name(font,name,id):font['name'].setName(name,nameID=id,platformID=3,platEncID=1,langID=1033)
 def get_font_name(font,id):return font['name'].getName(nameID=id,platformID=3,platEncID=1,langID=1033).__str__()
 def main(zip_path,target_path,config):
